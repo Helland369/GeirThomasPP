cecaf9a884b602bf04e7420324a1482c49d322a4
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2024-11-22 12:21:10 +0100

    Mer Rider spam

--- Program.cs
+++ Program.cs
@@ -1,63 +1,64 @@
-﻿
-
-
-class Program {
-
-
-   static void reverse()
+﻿namespace PP
+{
+    class Program
     {
 
-        Console.WriteLine("Sriv noe her!! Så blir den feil vei");
-
-        string x = Console.ReadLine();
-
-        for (int i = 0; i < x.Length; i++)
+        static void Main(string[] args)
+        {
+            bool visMeny = true;
+            while (visMeny)
+            {
+                visMeny = MainMenu();
+            }
+        }
+        static void reverse()
         {
 
-            Console.Write(x[x.Length - i - 1]);
+            Console.WriteLine("Skriv noe her!! Så blir den feil vei");
+
+            string x = Console.ReadLine();
+
+            for (int i = 0; i < x.Length; i++)
+            {
+
+                Console.Write(x[x.Length - i - 1]);
+            }
+        }
+
+        static void upCase()
+        {
+
+            Console.WriteLine("Skriv noe for å få det is store bokstaver!");
+
+            string y = Console.ReadLine().ToUpper();
+
+            Console.WriteLine(y);
+        }
+        static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Velg her: ");
+            Console.WriteLine("1) Trykk 1 for å reversere teksten");
+            Console.WriteLine("2) Trykk 2 for å få store bokstaver");
+            Console.WriteLine("3) Trykk 3 for å avslutte");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    reverse();
+                    return true;
+                case "2":
+                    upCase();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+                            } 
         }
     }
 
-    static void upCase() {
 
-        Console.WriteLine("Skriv noe for å få det is store bokstaver!");
-
-        string y = Console.ReadLine().ToUpper();
-
-        Console.WriteLine(y);
-    }
-    
-    static void Main() {
-
-        int input;
-
-        do
-        {
-            Console.WriteLine("1. Trykke 1 for å reversere teksten");
-            Console.WriteLine("2. Trykke 1 for å få store bokstaver");
-
-            input = Convert.ToInt32(Console.ReadLine());
-
-            switch (input) {
-                case 1:
-                    reverse();
-                    break;
-                case 2:
-                    upCase();
-                    break;
-                default:
-
-                    break;
-
-            }
-
-        } while (input != 0);
-
-    }
 }
-
-
-
 
 // Lag et program hvor du kan skrive inn en tekst, og når du sender teksten inn vil du få tilbake teksten reversert. Gjør dette uten å bruke innebygde reverse metoder
 // Bygg videre ved å legge til flere ting man kan gjøre med stringen, eksempel:
